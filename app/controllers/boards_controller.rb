db474bb42704a4469021c0869c4155e978944527
s8852s <s8852s@hotmail.com.tw>
2020-12-07 21:22:23 +0800

    CRUD

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,44 @@
+class BoardsController < ApplicationController
+  
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(params_board)
+    if @board.save
+      redirect_to root_path, notice: "成功新增看板"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @board = Board.find_by(id: params[:id])
+  end
+
+  def update
+    @board = Board.find_by(id: params[:id])
+    if @board.update(params_board)
+      redirect_to root_path, notice: "成功修改看板"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @board = Board.find_by(id: params[:id])
+    @board.destroy if @board
+    redirect_to root_path, notice: "成功刪除看板"
+  end
+
+  private
+  def params_board
+    params.require(:board).permit(:title)
+  end
+
+end
